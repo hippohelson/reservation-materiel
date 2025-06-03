6052ed3c2fa7e7f940a4288b5eeed2c4d24ce88d
hippohelson <hippolyte@helson.eu>
2025-06-03 10:42:17 +0200

    fix cdk + schema

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsconfig = {
+    aws_appsync_graphqlEndpoint: "https://pch5jfxwmjajjbaccuzercktzi.appsync-api.eu-west-3.amazonaws.com/graphql",
+    aws_appsync_region: "eu-west-3",
+    aws_appsync_authenticationType: "API_KEY",
+    aws_appsync_apiKey: "da2-vxwwj727r5cthfoptdo3xzs3xu", 
+  };
+  
+  export default awsconfig;
+  
